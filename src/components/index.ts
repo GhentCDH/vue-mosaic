188c7c180ccb342bc4bac05cf131299db4ac23d2
Frederic Lamsens <frederic.lamsens@ugent.be>
2026-02-20 22:26:27 +0100

    chore: add support for package build

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Mosaic } from './Mosaic.vue'
+export { default as MosaicCollection } from './MosaicCollection.vue'
+export { default as MosaicContext } from './MosaicContext.vue'
